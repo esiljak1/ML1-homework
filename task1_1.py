0c7bf41768c5c4244e64d95b87ac3990b16a80a5
Emin Siljak <esiljak1@etf.unsa.ba>
2022-05-27 13:49:16 +0200

    First attempt of knn implementation

--- task1_1.py
+++ task1_1.py
@@ -3,22 +3,34 @@ from sklearn.base import BaseEstimator
 
 
 class KNearestNeighborsClassifier(BaseEstimator):
-  def __init__(self, k=1):
-    self.k = k
+    def __init__(self, k=1):
+      self.y = None
+      self.X = None
+      self.k = k
 
-  def fit(self, X, y):
-    # TODO IMPLEMENT ME
-    # store X and y
-    return
+    def fit(self, X, y):
+        # TODO IMPLEMENT ME
+        # store X and y
+        self.X = X
+        self.y = y
+        return
 
-  def score(self, X, y):
-    y_pred = self.predict(X)
-    return np.mean(y_pred == y)
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        return np.mean(y_pred == y)
 
-  def predict(self, X):
-    # TODO: assign class labels
-    # useful numpy methods: np.argsort, np.unique, np.argmax, np.count_nonzero
-    # pay close attention to the `axis` parameter of these methods
-    # broadcasting is really useful for this task!
-    # See https://numpy.org/doc/stable/user/basics.broadcasting.html
-    return
+    def predict(self, X):
+        # TODO: assign class labels
+        # useful numpy methods: np.argsort, np.unique, np.argmax, np.count_nonzero
+        # pay close attention to the `axis` parameter of these methods
+        # broadcasting is really useful for this task!
+        # See https://numpy.org/doc/stable/user/basics.broadcasting.html
+        y_pred = []
+        for i in range(X.shape[0]):
+            distances = [np.linalg.norm(X[i] - self.X[j]) for j in range(self.X.shape[0])]
+            sorted_indices = np.argsort(distances)[:self.k]
+            neighbor_labels = self.y[sorted_indices]
+            labels, count = np.unique(neighbor_labels, return_counts=True)
+            max_label_index = np.argmax(count)
+            y_pred.append(labels[max_label_index])
+        return np.array(y_pred)
