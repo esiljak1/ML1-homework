9ff258bbc847ac183f56c9f0df8b534cdcba1ff9
Emin Siljak <esiljak1@etf.unsa.ba>
2022-05-29 12:55:52 +0200

    First part of the task 2.3 done

--- task2_3.py
+++ task2_3.py
@@ -7,13 +7,19 @@ import plotting
 from datasets import get_toy_dataset
 
 if __name__ == '__main__':
-  for idx in [1, 2, 3]:
-    X_train, X_test, y_train, y_test = get_toy_dataset(idx)
-    svc = SVC(tol=1e-4)
-    # TODO perform grid search, decide on suitable parameter ranges and state sensible parameter ranges in your report
-    clf = ...
-    clf.fit(X_train, y_train)
-    test_score = clf.score(X_test, y_test)
-    print("Test Score:", test_score)
-    print(f"Dataset {idx}: {clf.best_params_}")
-    # TODO plot and save decision boundaries
+    for idx in [1, 2, 3]:
+        X_train, X_test, y_train, y_test = get_toy_dataset(idx)
+        svc = SVC(tol=1e-4)
+        # TODO perform grid search, decide on suitable parameter ranges and state sensible parameter ranges in your report
+        # need to update the values of C and gamma
+        grid_dict = dict(
+          C=10**np.arange(-2.0, 2.0),
+          gamma=10**np.arange(-4.0, 2.0),
+          kernel=["linear", "rbf"]
+        )
+        clf = GridSearchCV(svc, grid_dict)
+        clf.fit(X_train, y_train)
+        test_score = clf.score(X_test, y_test)
+        print("Test Score:", test_score)
+        print(f"Dataset {idx}: {clf.best_params_}")
+        # TODO plot and save decision boundaries
