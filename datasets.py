2565fb939bb1a749d46edffc2c734fadaa9468ac
paloma del hierro <palomadelhierro2001@gmail.com>
2022-06-02 13:03:50 +0200

    Ignore changes in the file datasets

--- datasets.py
+++ datasets.py
@@ -1,3 +1,4 @@
+import os
 import pickle as pkl
 
 import numpy as np
@@ -6,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 def get_toy_dataset(dataset_idx, random_state=42, apply_noise=False, remove_outlier=False):
-  with open('data/dataset%d.pkl' % dataset_idx, 'rb') as f:
+  with open(f'{os.path.dirname(os.path.realpath(__file__))}/data/dataset{dataset_idx}.pkl', 'rb') as f:
     X, y = pkl.load(f)
 
   if remove_outlier:
