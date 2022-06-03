becd0d0cc61dcabb4cb62ff44cb8beaa6dd8e4ac
Emin Siljak <esiljak1@etf.unsa.ba>
2022-06-03 09:49:41 +0200

    Fixed the 2_1 predict

--- task2_1.py
+++ task2_1.py
@@ -1,24 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.base import BaseEstimator
-
-import plotting
-from datasets import get_toy_dataset
 
 
 def loss(w, b, C, X, y):
     # TODO: implement the loss function (eq. 1)
     sum = 0
     for i in range(y.shape[0]):
-        sum += max(0, 1 - y[i] * (np.dot(w.t, X[i]) + b))
-    return 0.5 * np.dot(w.t, w) + C * sum
+        sum += max(0, 1 - y[i] * (np.dot(w.T, X[i]) + b))
+    return 0.5 * np.dot(w.T, w) + C * sum
 
 
 def grad(w, b, C, X, y):
     # TODO: implement the gradients with respect to w and b.
     # useful methods: np.sum, np.where, numpy broadcasting
-    grad_w = w + C * np.sum([np.where(y[i] * (np.dot(w.t, X[i]) + b) > 1, 0, -y[i] * X[i]) for i in range(y.shape[0])])
-    grad_b = C * np.sum([np.where(y[i] * (np.dot(w.t, X[i]) + b) > 1, 0, -y[i]) for i in range(y.shape[0])])
+    grad_w = w + C * np.sum([np.where(y[i] * (np.dot(w.T, X[i]) + b) > 1, 0, -y[i] * X[i]) for i in range(y.shape[0])]) / y.shape[0]
+    grad_b = C * np.sum([np.where(y[i] * (np.dot(w.T, X[i]) + b) > 1, 0, -y[i]) for i in range(y.shape[0])]) / y.shape[0]
     return grad_w, grad_b
 
 
@@ -33,24 +29,26 @@ class LinearSVM(BaseEstimator):
         # TODO: initialize w and b. Does the initialization matter?
         # convert y: {0,1} -> -1, 1
         y = np.where(y == 0, -1, 1)
-        self.w = np.random.normal(size=X.shape[1])
+        self.w = np.zeros(X.shape[1])
         self.b = 0.
         loss_list = []
-
+        VALUE = 10
         for j in range(self.max_iter):
             # TODO: compute the gradients, update the weights, compute the loss
             grad_w, grad_b = grad(self.w, self.b, self.C, X, y)
             self.w = self.w - self.eta * grad_w
             self.b = self.b - self.eta * grad_b
             loss_list.append(loss(self.w, self.b, self.C, X, y))
+            if loss_list[j] < VALUE:
+                return loss_list
 
         return loss_list
 
     def predict(self, X):
         # TODO: assign class labels to unseen data
-        y_pred = np.dot(self.w.T, X) + self.b
+        y_pred = [np.sign(np.dot(self.w.T, X[i]) + self.b) for i in range(X.shape[0])]
         # converting y_pred from {-1, 1} to {0, 1}
-        return np.where(y_pred == -1, 0, 1)
+        return [np.where(y_pred[i] == -1, 0, 1) for i in range(len(y_pred))]
 
     def score(self, X, y):
         # TODO: IMPLEMENT ME
--- task2_2.py
+++ task2_2.py
@@ -4,21 +4,32 @@ import plotting
 from datasets import get_toy_dataset
 from task2_1 import LinearSVM
 from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 if __name__ == '__main__':
-  X_train, X_test, y_train, y_test = get_toy_dataset(1, remove_outlier=True)
-  svm = LinearSVM()
-  # TODO use grid search to find suitable parameters!
-  clf = ...
-  clf.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = get_toy_dataset(1, remove_outlier=True)
+    svm = LinearSVM()
+    # TODO use grid search to find suitable parameters!
 
-  # TODO Use the parameters you have found to instantiate a LinearSVM.
-  # the `fit` method returns a list of scores that you should plot in order
-  # to monitor the convergence. When does the classifier converge?
-  svm = LinearSVM(...)
-  scores = svm.fit(X_train, y_train)
-  plt.plot(scores)
-  test_score = clf.score(X_test, y_test)
-  print(f"Test Score: {test_score}")
+    param_grid = dict(
+      C=10.**np.arange(-4, 4),
+      eta=10.**np.arange(-4, -1)
+    )
 
-  # TODO plot the decision boundary!
+    clf = GridSearchCV(svm, param_grid)
+    clf.fit(X_train, y_train)
+
+    clf.score(X_test, y_test)
+    print(f"Best parameters: {clf.best_params_}, best value: {clf.best_score_}")
+
+    # TODO Use the parameters you have found to instantiate a LinearSVM.
+    # the `fit` method returns a list of scores that you should plot in order
+    # to monitor the convergence. When does the classifier converge?
+    svm = LinearSVM(C=clf.best_params_["C"], eta=clf.best_params_["eta"])
+    scores = svm.fit(X_train, y_train)
+    plt.plot(scores)
+    plt.show()
+    test_score = svm.score(X_test, y_test)
+    print(f"Test Score: {test_score}")
+
+    # TODO plot the decision boundary!
